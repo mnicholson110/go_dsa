f396c539aa2968077b4a3df91f628100281ca54e
americanjetset <mnicholson110@gmail.com>
2023-10-03 18:39:34 -0500

    updated lru tests

--- lrucache/lrucache_test.go
+++ lrucache/lrucache_test.go
@@ -12,22 +12,30 @@ func TestIntLRUCache(t *testing.T) {
 	}
 
 	c.Update("foo", 69)
-	if _, r := c.Get("foo"); !r {
+
+	v, r := c.Get("foo")
+	if v != 69 || !r {
 		t.Error("Expected 69")
 	}
 
 	c.Update("bar", 420)
-	if _, r := c.Get("bar"); !r {
+
+	v, r = c.Get("bar")
+	if v != 420 || !r {
 		t.Error("Expected 420")
 	}
 
 	c.Update("baz", 1337)
-	if _, r := c.Get("baz"); !r {
+
+	v, r = c.Get("baz")
+	if v != 1337 || !r {
 		t.Error("Expected 1337")
 	}
 
 	c.Update("ball", 69420)
-	if _, r := c.Get("ball"); !r {
+
+	v, r = c.Get("ball")
+	if v != 69420 || !r {
 		t.Error("Expected 69420")
 	}
 
@@ -35,17 +43,19 @@ func TestIntLRUCache(t *testing.T) {
 		t.Error("Expected nil")
 	}
 
-	if _, r := c.Get("bar"); !r {
+	v, r = c.Get("bar")
+	if v != 420 || !r {
 		t.Error("Expected 420")
 	}
 
 	c.Update("foo", 69)
 
-	if _, r := c.Get("bar"); !r {
+	if v != 420 || !r {
 		t.Error("Expected 420")
 	}
 
-	if _, r := c.Get("foo"); !r {
+	v, r = c.Get("foo")
+	if v != 69 || !r {
 		t.Error("Expected 69")
 	}
 
